f83d87c9cf1f1f620996abb6e32332e75a8af812
Adrian Martinez <adrian@adrians-mac-mini.local>
2019-10-27 22:01:15 +0100

    challenge: actualizar timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -12,10 +12,11 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     
-    @IBOutlet weak var progresBar: UIProgressView!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     let eggTimes : [String : Int] = ["Hard": 12 * 1 , "Medium": 7 * 1 , "Soft": 5 * 1]
     
+    var initialCountDown : Int = 0
     var countDown : Int = 0
     var timer = Timer()
     
@@ -24,18 +25,20 @@ class ViewController: UIViewController {
 
         if let result = eggTimes[hardness] {
             countDown = result
+            initialCountDown = countDown
         }
         
        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(update), userInfo: nil, repeats: true)
  
-        progresBar.progress = 1.0
+        progressBar.progress = 1.0
     }
     
     @objc func update() {
         if (countDown > 0) {
             print("\(countDown) seconds")
             countDown -= 1
-            titleLabel.text = "\(countDown) s"
+//            titleLabel.text = "\(countDown) s"
+            progressBar.progress = Float(countDown) / Float(initialCountDown)
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
